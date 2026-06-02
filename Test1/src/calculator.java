1f954fef3956b4f2e9d83f42d7eebe6c33cf1f8e
[sumxinee] <[ksvmxx@gmail.com]>
2026-06-02 11:09:39 +0900

    add calculator file

---
+++ Test1/src/calculator.java
@@ -0,0 +1,11 @@
+public class calculator {
+    // 더하기 기능
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        calculator calc = new calculator();
+        System.out.println("1 + 2 = " + calc.add(1, 2));
+    }
+}
